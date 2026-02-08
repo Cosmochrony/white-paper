774fe3adb9f8e86b63b27185fbe404a0d4cf61bc
javarome <javarome@gmail.com>
2026-02-08 14:33:39 +0100

    Standardize bibliography formatting: align field indentation and add "address" field to book entries; add `compile.sh` script for build automation; regenerate `Cosmochrony.pdf`.

---
+++ compile.sh
@@ -0,0 +1,4 @@
+pdflatex -file-line-error -interaction=nonstopmode -synctex=1 \
+  -output-directory=/Users/javarome/project/@cosmochrony/white-paper/out \
+  -recorder \
+  /Users/javarome/project/@cosmochrony/white-paper/tex/Cosmochrony.tex%
